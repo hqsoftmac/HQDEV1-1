776ab5f27365671c4ea3e4f4e5279d88956e3110
hqsoftmac <5413873@qq.com>
2014-04-11 09:55:20 +0800

    新增OnLineExam类库

---
+++ HQOnlineExam/ML/OEContentClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HQOnlineExam.ML
+{
+    [Serializable]
+    public class OEContentClass 
+    {
+        public Int64 FContentClassId { get; set; }
+
+        public string FContentClassCode { get; set; }
+
+        public string FContentClassName { get; set; }
+
+        public string FContentClassContent { get; set; }
+
+        public Int64 FParentId { get; set; }
+
+        public OEContentClass()
+        {
+
+        }
+
+
+
+    }
+}
