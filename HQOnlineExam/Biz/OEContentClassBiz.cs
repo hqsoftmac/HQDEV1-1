56a84abda85c7bac627deb900a977f0fa19eedb8
rdzglijb <rdzglijb@126.com>
2014-04-13 16:29:24 +0800

    ljb

---
+++ HQOnlineExam/Biz/OEContentClassBiz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HQOnlineExam.ML;
+using HQLib.Common;
+using System.Collections.Specialized;
+using HQOnlineExam.DA;
+using HQLib;
+using HQConst.Const;
+
+namespace HQOnlineExam.Biz
+{
+    public class OEContentClassBiz
+    {
+
+        public List<OEContentClass> Select(NameValueCollection where)
+        {
+            OEContentClassDA da = new OEContentClassDA();
+            return da.Select(where).DataTableToList<OEContentClass>();
+        }
+
+        public List<OEContentClass> Select(NameValueCollection where, NameValueCollection orderby)
+        {
+            OEContentClassDA da = new OEContentClassDA();
+            return da.Select(where, orderby).DataTableToList<OEContentClass>();
+        }
+    }
+}
