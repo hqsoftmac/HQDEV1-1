de2680a9388a59ca68dbea6a0cde77c2d3ee130a
hqsoftmac <5413873@qq.com>
2014-04-17 11:10:32 +0800

    完成OEContentClassTree及其Biz

---
+++ HQOnlineExam/Biz/OEContentClassTreeBiz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HQOnlineExam.ML;
+using System.Web.Script.Serialization;
+
+namespace HQOnlineExam.Biz
+{
+    public class OEContentClassTreeBiz
+    {
+        public List<OEContentClassTree> Select()
+        {
+            OEContentClassBiz biz = new OEContentClassBiz();
+            return Select(0,biz.Select());
+        }
+
+        public string JsonSelect()
+        {
+            return ConvertToJson(Select());
+        }
+
+        private List<OEContentClassTree> Select(Int64 _parentid, List<OEContentClass> _list)
+        {
+            List<OEContentClassTree> lists = new List<OEContentClassTree>();
+            foreach (OEContentClass item in _list.Where(p => p.FParentId == _parentid))
+            {
+                OEContentClassTree newitem = new OEContentClassTree();
+                newitem.FContentClassId = item.FContentClassId;
+                newitem.FContentClassCode = item.FContentClassCode;
+                newitem.FContentClassName = item.FContentClassName;
+                newitem.FContentClassContent = item.FContentClassContent;
+                newitem.FIconPath = item.FIconPath;
+                newitem.FParentId = item.FParentId;
+                newitem.children = Select(item.FContentClassId, _list);
+                lists.Add(newitem);
+            }
+            return lists;
+        }
+
+        public string ConvertToJson(List<OEContentClassTree> lists)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return "{\"total\":" + lists.Count.ToString() + ",\"rows\":" + serializer.Serialize(lists) + "}";
+        }
+
+    }
+}
---
+++ HQOnlineExam/ML/OEContentClassTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HQLib.Interface;
+using HQLib.Util;
+
+namespace HQOnlineExam.ML
+{
+    [Serializable]
+    public class OEContentClassTree :OEContentClass ,IDatasource
+    {
+        public List<OEContentClassTree> children { get; set; }
+
+        public OEContentClassTree()
+        {
+
+        }
+
+        string IDatasource.ToJson()
+        {
+            return Utils.ConvertToJson(this);
+        }
+    }
+}
